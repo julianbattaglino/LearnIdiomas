d392f5a94513fd5b60c5f1cb73164abe39580667
Julian Battaglino <julianbattaglino@gmail.com>
2022-09-06 03:31:09 +0200

    recaptcha jquery functions

--- scripts.js
+++ scripts.js
@@ -1,32 +1,53 @@
 // slick Init //
-    $('.slick-slider').slick({
-dots: true,
-speed: 300,
-slidesToShow: 3,
-centerMode: false,
-focusOnSelect: true,
-zIndex: 999999,
-responsive: [
-  {
-    breakpoint: 1024,
-    settings: {
-      slidesToShow: 3,
-      infinite: true,
-      dots: true
-    }
-  },
-  {
-    breakpoint: 767,
-    settings: {
-      slidesToShow: 1,
-    }
-  },
-  // You can unslick at a given breakpoint now by adding:
-  // settings: "unslick"
-  // instead of a settings object
-]
+$('.slick-slider').slick({
+  dots: true,
+  speed: 300,
+  slidesToShow: 3,
+  centerMode: false,
+  focusOnSelect: true,
+  adaptiveHeight: true,
+  zIndex: 999999,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 
-}); 
+});
 
 // Aos Init //
-  AOS.init();
+AOS.init();
+
+/* Recaptcha */
+function recaptchaCallback() {
+  $('#submitBtn').prop('disabled', false);
+  $('#verifica').hide();
+  $('#sucess').show();
+}
+
+function recaptchaExpiredCallback() {
+  // Resetear recaptcha en caso de que el captcha expire
+  $('#submitBtn').prop('disabled', true);
+  grecaptcha.reset();
+  $('#sucess').hide();
+}
+
+function recaptchaErrorCallback() {
+  // Resetear recaptcha en caso de error por network connectivity
+  $('#submitBtn').prop('disabled', true);
+  grecaptcha.reset();
+}
